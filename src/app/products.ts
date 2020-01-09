39e5c57783bb9c0237b3da074a6c9717a8c30605
lyp830414 <lyp830414@163.com>
2020-01-09 18:53:31 +0800

    push codes

---
+++ src/app/products.ts
@@ -0,0 +1,15 @@
+export const products = [
+  {
+    name: 'Zijing Zhang : 799ETH',
+    balance: 799,
+    description: 'Display user balance'
+  },
+
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
